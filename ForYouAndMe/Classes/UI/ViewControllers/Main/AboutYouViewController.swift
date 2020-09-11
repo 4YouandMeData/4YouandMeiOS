4eea8f29cf3dded5744da198626f60af368a593c
Giuseppe Lapenta <giuseppe@balzo.eu>
2020-09-11 21:10:06 +0200

    FYAM-322 Add Icon

---
+++ ForYouAndMe/Classes/UI/ViewControllers/Main/AboutYouViewController.swift
@@ -0,0 +1,8 @@
+//
+//  AboutYouViewController.swift
+//  ForYouAndMe
+//
+//  Created by Giuseppe Lapenta on 11/09/2020.
+//
+
+import Foundation
---
+++ ForYouAndMe/Classes/UI/Views/AboutYouHeaderView.swift
@@ -0,0 +1,8 @@
+//
+//  AboutYouHeaderView.swift
+//  ForYouAndMe
+//
+//  Created by Giuseppe Lapenta on 11/09/2020.
+//
+
+import Foundation
