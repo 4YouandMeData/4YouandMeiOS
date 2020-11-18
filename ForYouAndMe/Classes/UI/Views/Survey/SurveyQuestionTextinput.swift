3ee29e0d3730ee8fe235f888a64dd2077f86837f
Leonardo Passeri <leonardo@balzo.eu>
2020-11-18 18:47:15 +0100

    FYAM-552: Survey text input: added keyboard toolbar with done button to dismiss the keyboard.

--- ForYouAndMe/Classes/UI/Views/Survey/SurveyQuestionTextinput.swift
+++ ForYouAndMe/Classes/UI/Views/Survey/SurveyQuestionTextinput.swift
@@ -9,7 +9,47 @@ class SurveyQuestionTextInput: UIView, UITextViewDelegate {
     
     private var surveyQuestion: SurveyQuestion
     private var answer: String = String()
-    private var labelLimit: UILabel = {
+    
+    private lazy var textView: UITextView = {
+        
+        // Text View
+        let textView = UITextView()
+        let style = NSMutableParagraphStyle()
+        style.lineSpacing = 8
+        textView.typingAttributes = [.foregroundColor: ColorPalette.color(withType: .primaryText),
+                                     .font: FontPalette.fontStyleData(forStyle: .header3).font,
+                                     .paragraphStyle: style]
+        textView.delegate = self
+        textView.layer.borderWidth = 1
+        textView.tintColor = ColorPalette.color(withType: .primary)
+        textView.layer.borderColor = ColorPalette.color(withType: .inactive).cgColor
+        
+        // Toolbar
+        let toolBar = UIToolbar()
+        toolBar.barStyle = .default
+        toolBar.isTranslucent = true
+        toolBar.tintColor = ColorPalette.color(withType: .primary)
+        toolBar.sizeToFit()
+        
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(self.doneButtonPressed))
+        let spaceButton = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        
+        toolBar.setItems([spaceButton, doneButton], animated: false)
+        textView.inputAccessoryView = toolBar
+        
+        return textView
+    }()
+    
+    private lazy var placeholderLabel: UILabel = {
+        let label = UILabel()
+        label.text = self.surveyQuestion.placeholder
+        label.font = FontPalette.fontStyleData(forStyle: .paragraph).font
+        label.textColor = ColorPalette.color(withType: .inactive)
+        label.sizeToFit()
+        return label
+    }()
+    
+    private var limitLabel: UILabel = {
         let label = UILabel()
         label.textAlignment = .right
         label.font = FontPalette.fontStyleData(forStyle: .header3).font
@@ -17,12 +57,12 @@ class SurveyQuestionTextInput: UIView, UITextViewDelegate {
         return label
     }()
     
-    private var placeholderLabel: UILabel!
-    private var maxCharacters: Int = 0
+    private let maxCharacters: Int
     private static let optionWidth: CGFloat = 74.0
     private weak var delegate: SurveyQuestionProtocol?
     
     init(surveyQuestion: SurveyQuestion, delegate: SurveyQuestionProtocol) {
+        self.maxCharacters = surveyQuestion.maxCharacters ?? 250
         self.surveyQuestion = surveyQuestion
         self.delegate = delegate
         
@@ -34,32 +74,23 @@ class SurveyQuestionTextInput: UIView, UITextViewDelegate {
         stackView.autoPinEdgesToSuperviewEdges()
         
         stackView.addBlankSpace(space: 30)
-        self.placeholderLabel = UILabel()
-        self.placeholderLabel.text = self.surveyQuestion.placeholder
-        self.placeholderLabel.font = FontPalette.fontStyleData(forStyle: .paragraph).font
-        self.placeholderLabel.textColor = ColorPalette.color(withType: .inactive)
-        self.placeholderLabel.sizeToFit()
-        let textView = UITextView()
-        let style = NSMutableParagraphStyle()
-        style.lineSpacing = 8
-        textView.typingAttributes = [.foregroundColor: ColorPalette.color(withType: .primaryText),
-                                     .font: FontPalette.fontStyleData(forStyle: .header3).font,
-                                     .paragraphStyle: style]
-        textView.delegate = self
-        textView.layer.borderWidth = 1
-        textView.tintColor = ColorPalette.color(withType: .primary)
-        textView.layer.borderColor = ColorPalette.color(withType: .inactive).cgColor
-        textView.addSubview(self.placeholderLabel)
+        
+        // TextView
+        stackView.addArrangedSubview(self.textView)
+        
+        // Placeholder label
+        self.textView.addSubview(self.placeholderLabel)
         self.placeholderLabel.isHidden = !textView.text.isEmpty
-        self.placeholderLabel.autoPinEdgesToSuperviewEdges(with: UIEdgeInsets(top: CGFloat(textView.font!.pointSize/2),
+        self.placeholderLabel.autoPinEdgesToSuperviewEdges(with: UIEdgeInsets(top: CGFloat(self.textView.font!.pointSize/2),
                                                                               left: 5,
                                                                               bottom: 10,
                                                                               right: 10))
-        self.maxCharacters = self.surveyQuestion.maxCharacters ?? 250
-        stackView.addArrangedSubview(textView)
+        
         stackView.addBlankSpace(space: 5)
-        self.labelLimit.text = "\(textView.text.count) / \(self.maxCharacters)"
-        stackView.addArrangedSubview(self.labelLimit)
+        
+        // Limit label
+        self.limitLabel.text = "\(self.textView.text.count) / \(self.maxCharacters)"
+        stackView.addArrangedSubview(self.limitLabel)
         
         stackView.addBlankSpace(space: 15)
         
@@ -70,19 +101,27 @@ class SurveyQuestionTextInput: UIView, UITextViewDelegate {
         fatalError("init(coder:) has not been implemented")
     }
     
+    // MARK: - Actions
+    
+    @objc private func doneButtonPressed() {
+        self.textView.resignFirstResponder()
+    }
+    
+    // MARK: - UITextViewDelegate
+    
     func textViewDidBeginEditing(_ textView: UITextView) {
         self.placeholderLabel.isHidden = !textView.text.isEmpty
     }
     
     func textViewDidChange(_ textView: UITextView) {
         self.placeholderLabel.isHidden = !textView.text.isEmpty
-        self.labelLimit.text = "\(textView.text.count) / \(self.maxCharacters)"
+        self.limitLabel.text = "\(textView.text.count) / \(self.maxCharacters)"
         if textView.text.count <= self.maxCharacters {
             textView.layer.borderColor = ColorPalette.color(withType: .inactive).cgColor
-            self.labelLimit.textColor = ColorPalette.color(withType: .inactive)
+            self.limitLabel.textColor = ColorPalette.color(withType: .inactive)
         } else {
             textView.layer.borderColor = UIColor.red.cgColor
-            self.labelLimit.textColor = .red
+            self.limitLabel.textColor = .red
         }
         
         self.delegate?.answerDidChange(self.surveyQuestion, answer: textView.text ?? "")
