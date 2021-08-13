e94cca0ca325fcda6d703b4b0adee9711cb7daf0
giuslape <giuseppe@balzo.eu>
2021-08-13 14:30:13 +0200

    Fix timezone

--- ForYouAndMe/Classes/Entities/Feed.swift
+++ ForYouAndMe/Classes/Entities/Feed.swift
@@ -180,8 +180,7 @@ struct FeedResolvedTemplate: Decodable {
             dateFormatterGet.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
             let someDate = resolvedString
             if dateFormatterGet.date(from: someDate) != nil {
-                self.resolved = dateFormatterGet.date(from: someDate)?.string(withFormat: "MMM dd",
-                                                                              timeZone: TimeZone(abbreviation: "UTC"))
+                self.resolved = dateFormatterGet.date(from: someDate)?.string(withFormat: "MMM dd")
             } else {
                 self.resolved = resolvedString
             }
