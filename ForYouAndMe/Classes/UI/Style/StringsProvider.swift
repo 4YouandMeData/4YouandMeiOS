4531431b454144a8ebe77b48e6ee24ead40024da
Iacopo Checcacci <iacopo@balzo.eu>
2023-04-12 10:51:53 +0200

    Quick activities counter label empty case handled

--- ForYouAndMe/Classes/UI/Style/StringsProvider.swift
+++ ForYouAndMe/Classes/UI/Style/StringsProvider.swift
@@ -121,6 +121,7 @@ enum StringKey: String, CaseIterable, CodingKey {
     case quickActivityButtonDefault = "QUICK_ACTIVITY_BUTTON_DEFAULT"
     case quickActivityButtonNext = "QUICK_ACTIVITY_BUTTON_NEXT"
     case quickActivityTotalNumber = "QUICK_ACTIVITIES_TOTAL_NUMBER"
+    case quickActivitiesRemaining = "QUICK_ACTIVITIES_REMAINING"
     // Notifiable
     case educationalButtonDefault = "EDUCATIONAL_BUTTON_DEFAULT"
     case rewardButtonDefault = "REWARD_BUTTON_DEFAULT"
--- ForYouAndMe/Classes/UI/Views/QuickActivityListTableViewCell.swift
+++ ForYouAndMe/Classes/UI/Views/QuickActivityListTableViewCell.swift
@@ -123,20 +123,27 @@ class QuickActivityListTableViewCell: UITableViewCell {
     
     private func updatePageControl() {
         var totalPagesCount = 4 // Default quick activity total number
-        if let dynamicTotalPageCount = Int(StringsProvider.string(forKey: .quickActivityTotalNumber)), dynamicTotalPageCount > 0 {
-            totalPagesCount = dynamicTotalPageCount
+        if let dynamicTotalPageCount = Int(StringsProvider.string(forKey: .quickActivityTotalNumber)) {
+            if dynamicTotalPageCount > 0 {
+                totalPagesCount = dynamicTotalPageCount
+            }
+            let currentPage = totalPagesCount - ((self.items.count - 1) % totalPagesCount)
+            
+            let currentPageAttributedText = NSMutableAttributedString.create(withText: "\(currentPage)",
+                                                                             fontStyle: .paragraph,
+                                                                             colorType: .primaryText)
+            let totalPageAttributedText =  NSAttributedString.create(withText: " / \(totalPagesCount)",
+                                                                     fontStyle: .paragraph,
+                                                                     color: ColorPalette.color(withType: .fourthText).applyAlpha(0.3))
+            let pageControlAttributedText = NSMutableAttributedString(attributedString: currentPageAttributedText)
+            pageControlAttributedText.append(totalPageAttributedText)
+            self.pageControlLabel.attributedText = pageControlAttributedText
+        } else {
+            let remainingText = StringsProvider.string(forKey: .quickActivitiesRemaining)
+            self.pageControlLabel.attributedText = NSMutableAttributedString.create(withText: "\(self.items.count) \(remainingText)",
+                                                                                    fontStyle: .paragraph,
+                                                                                    colorType: .primaryText)
         }
-        let currentPage = totalPagesCount - ((self.items.count - 1) % totalPagesCount)
-
-        let currentPageAttributedText = NSMutableAttributedString.create(withText: "\(currentPage)",
-               fontStyle: .paragraph,
-               colorType: .primaryText)
-        let totalPageAttributedText =  NSAttributedString.create(withText: " / \(totalPagesCount)",
-            fontStyle: .paragraph,
-            color: ColorPalette.color(withType: .fourthText).applyAlpha(0.3))
-        let pageControlAttributedText = NSMutableAttributedString(attributedString: currentPageAttributedText)
-        pageControlAttributedText.append(totalPageAttributedText)
-        self.pageControlLabel.attributedText = pageControlAttributedText
     }
 }
 
