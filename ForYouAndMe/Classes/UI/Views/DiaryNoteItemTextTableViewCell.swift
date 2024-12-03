519904d4219b395177b4de82d29918edc7d515b1
giuslape <giuseppe@balzo.eu>
2024-12-03 11:00:39 +0100

    Added Images and changed name of cell

--- ForYouAndMe/Classes/UI/Views/DiaryNoteItemTextTableViewCell.swift
+++ ForYouAndMe/Classes/UI/Views/DiaryNoteItemTextTableViewCell.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class DiaryNoteItemTableViewCell: UITableViewCell {
+class DiaryNoteItemTextTableViewCell: UITableViewCell {
     
     private lazy var noteImageView: UIImageView = {
         let imageView = UIImageView()
