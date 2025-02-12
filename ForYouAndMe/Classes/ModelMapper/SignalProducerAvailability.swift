062d04867433b7de6999c65e67a8ae8e44bcf57e
giuslape <giuseppe@balzo.eu>
2025-02-12 09:41:07 +0100

    Move file to Classes in pod

---
+++ ForYouAndMe/Classes/ModelMapper/SignalProducerAvailability.swift
@@ -0,0 +1,15 @@
+import ReactiveSwift
+import Moya
+#if !COCOAPODS
+    import Moya_ModelMapper
+#endif
+
+/// Extension for processing Responses into Mappable objects through ObjectMapper
+extension SignalProducerProtocol where Value == Moya.Response, Error == MoyaError {
+
+    @available(*, unavailable, renamed: "map(to:keyPath:)")
+    public func mapObject<T: Mappable>(type: T.Type, keyPath: String? = nil) -> SignalProducer<T, MoyaError> { fatalError() }
+
+    @available(*, unavailable, renamed: "map(to:keyPath:)")
+    public func mapArray<T: Mappable>(type: T.Type, keyPath: String? = nil) -> SignalProducer<[T], MoyaError> { fatalError() }
+}
